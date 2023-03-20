7998411dc8681c23ae3a18ff8c21fa87b8a3d3f6
philip dutra <monsterphilip10@gmail.com>
2023-03-19 21:40:15 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event){
+        let cards = [];
+       for(let i=0; i < name.length; i++ ){
+            cards[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+    }
+    return cards;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+
+}
